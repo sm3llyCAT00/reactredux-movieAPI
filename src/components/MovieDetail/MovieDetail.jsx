9a26854c81b177bf07bf421e777d9799dce8be89
sm3llyCAT00 <pis.an6991@gmail.com>
2023-05-27 01:35:15 +0800

    Fix some state for reducer.

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';   
-import { fetchAsyncMovieOrShowDetail, getSelectedMovieOrShow } from '../../features/movies/movieSlice';
+import { useParams } from 'react-router-dom';
+import {
+    fetchAsyncMovieOrShowDetail,
+    getSelectedMovieOrShow,
+    removeSelectedMovieOrShow
+} from '../../features/movies/movieSlice';
+import "./MovieDetail.scss";
 
 const MovieDetail = () => {
     const { imdbID } = useParams();
@@ -10,11 +15,64 @@ const MovieDetail = () => {
     console.log(data);
     useEffect(() => {
         dispatch(fetchAsyncMovieOrShowDetail(imdbID))
+        return () => {
+            dispatch(removeSelectedMovieOrShow());
+        }
     }, [dispatch, imdbID]);
 
     return (
-        <div>
-            MovieDetail
+        <div className="movie-section">
+            {Object.keys(data).length === 0 ?
+            (<div>...Loading</div>)
+            : (
+            <>
+                <div className="selection-left">
+                    <div className="movie-title">
+                        {data.Title}
+                    </div>
+                    <div className="movie-rating">
+                        <span>
+                            IMDb Rating <i className="fa fa-star"></i> : {data.imdbRating}
+                        </span>
+                        <span>
+                            IMDb Votes <i className="fa fa-thumbs-up"></i> : {data.imdbVotes}
+                        </span>
+                        <span>
+                            Runtime <i className="fa fa-film"></i> : {data.Runtime}
+                        </span>
+                        <span>
+                            Year <i className="fa fa-calendar"></i> : {data.Year}
+                        </span>
+                    </div>
+                    <div className="movie-plot">{data.Plot}</div>
+                    <div className="movie-info">
+                        <div>
+                            <span>Director</span>
+                            <span>{data.Director}</span>
+                        </div>
+                        <div>
+                            <span>Stars</span>
+                            <span>{data.Actors}</span>
+                        </div>
+                        <div>
+                            <span>Genres</span>
+                            <span>{data.Genre}</span>
+                        </div>
+                        <div>
+                            <span>Languages</span>
+                            <span>{data.Language}</span>
+                        </div>
+                        <div>
+                            <span>Awards</span>
+                            <span>{data.Awards}</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="section-right">
+                    <img src={data.Poster} alt={data.Title} />
+                </div>
+            </>
+            )}
         </div>
     );
 };
